0551bcd3ec79565d7f63ecb4e4204fb259b1e148
Dodzi Y. Dzakuma <nexocentric@gmail.com>
2015-10-23 04:02:34 -0500

    Registering tasks works just fine. I now need to add safety checks to make sure that the task does not already exist and figure out a way to allow users to register the task if and only if they know the administrator password.

--- functions/Register-PseudoService.ps1
+++ functions/Register-PseudoService.ps1
@@ -10,11 +10,14 @@ function Register-PseudoService
 		[ValidateNotNullOrEmpty()]
 		[parameter(Mandatory=$true)]
 		[ValidateScript({Test-Path -Path $_ -PathType Leaf})]
-		[string]$ExecutableFile
+		[string]$ExecutableFile,
+
+		[ValidateNotNullOrEmpty()]
+		[string]$Description
 	)
 
 	Write-Verbose -Message ("Performing checks for proper credentials.")
-	# $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+	$currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
 	# if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator))
 	# {
 	# 	Write-Error -Message ("You must run PowerShell as administrator to run this function.")
@@ -26,10 +29,12 @@ function Register-PseudoService
 
 	Write-Verbose -Message ("Script confirmed at location: ${ExecutableFile}")
 
-	$taskName = "$Name"
+	$taskName = "${Name}"
 	$taskAction = New-ScheduledTaskAction -Execute $ExecutableFile
+
+	$creationDate = Get-Date
 	
-	$taskTrigger = New-ScheduledTaskTrigger -Once:$false -At 6AM -RepetitionDuration ([TimeSpan]::MaxValue) -RepetitionInterval (New-TimeSpan -Minutes 5)
+	$taskTrigger = New-ScheduledTaskTrigger -Once:$false -At $creationDate -RepetitionDuration ([TimeSpan]::MaxValue) -RepetitionInterval (New-TimeSpan -Minutes 5)
 
 # (New-TimeSpan -Minutes 5)
 	$taskSettings = New-ScheduledTaskSettingsSet -RestartCount 3 -RestartInterval (New-TimeSpan -Minutes 1) -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -DontStopOnIdleEnd -WakeToRun
@@ -37,9 +42,18 @@ function Register-PseudoService
 
 	# $userPassword = Read-Host -Prompt ("Please enter the password for [${taskUser}]") -AsSecureString
 
-	$taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+	$taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+	$dom = $env:userdomain
+	$usr = $env:username
+	
+	$creationDate = Get-Date
+	$Description = "This task was created by the Register-PseudoService PowerShell cmdlet on [${creationDate}].`n`n${Description}"
 
 	$scheduledTask = New-ScheduledTask -Action $taskAction -Principal $taskPrincipal -Trigger $taskTrigger -Settings $taskSettings
+	$scheduledTask.Author = ([adsi]"WinNT://$dom/$usr,user").FullName
+	$scheduledTask.Description = $Description
+	$scheduledTask.Date = $creationDate
 
 	Register-ScheduledTask -TaskName $taskName -InputObject $scheduledTask
 }
--- functions/Unregister-PseudoService.ps1
+++ functions/Unregister-PseudoService.ps1
@@ -7,6 +7,18 @@ function Unregister-PseudoService
 		# [ValidateScript({Test-Path -Path $_ -PathType Leaf})]
 		[string]$Name
 	)
+	try
+	{
+		Write-Verbose -Message ("Checking for pseduo service [${Name}].")
+		$scheduledTaskObject = Get-ScheduledTask -TaskName $Name
 
-	Unregister-ScheduledTask -TaskName $Name
+		Write-Verbose -Message ("Pseduo service [${Name}] found!")
+		Unregister-ScheduledTask -TaskName $Name
+		return $true
+	}
+	catch
+	{
+		Write-Verbose -Message ("No pseduo service by the name of [${Name}] exists.")
+		return $false
+	}
 }
