166ea140c2ebce5bde28ae62723285bece6398a5
Dodzi Y. Dzakuma <nexocentric@gmail.com>
2015-10-25 21:14:46 -0500

    Added a few more steps for installation.

--- Install-PoshPseudoServiceManagement.ps1
+++ Install-PoshPseudoServiceManagement.ps1
@@ -31,15 +31,35 @@ function Install-PoshPseudoServiceManagement
 
 	if ($PSCmdlet.ShouldProcess("thing", "place"))
 	{
-		Write-Verbose -Message ("Simulate download")
-		# (New-Object Net.WebClient).DownloadFile(
-		# 	"https://github.com/nexocentric/posh-pseudo-service-management/archive/1.0.0.zip",
-		# 	"${installationPath}\${installationDirectory}\posh-pseudo-service-management-1.0.0.zip" #full file path required!
-		# )
+		Write-Verbose -Message ("Downloading archive")
+		(New-Object Net.WebClient).DownloadFile(
+			"https://github.com/nexocentric/posh-pseudo-service-management/archive/1.0.0.zip",
+			"${installationPath}\${installationDirectory}\posh-pseudo-service-management-1.0.0.zip" #full file path required!
+		)
 	}
 	else {
-		Write-Verbose -Message ("Download!")
+		Write-Verbose -Message ("Simulating download!")
 	}
+
+	$previousLocation = (Get-Location).Path
+	if ($PSCmdlet.ShouldProcess("thing", "place"))
+	{
+		$fullFileName = "${installationPath}\${installationDirectory}\posh-pseudo-service-management-1.0.0.zip"
+		Write-Verbose -Message ("Extracting archive!")
+		Set-Location -Path "${installationPath}\${installationDirectory}"
+		$shell = New-Object -com shell.application
+		$zip = $shell.NameSpace($fullFileName)
+		foreach($item in $zip.items())
+		{
+			$shell.Namespace($fullFileName.Replace(".zip", "").Replace($ReleaseName, "")).copyhere($item)
+		}
+
+		Remove-Item -Path $fullFileName
+	}
+	else {
+		Write-Verbose -Message ("Simulating archive extraction!")
+	}
+	Set-Location -Path $previousLocation
 }
 
 Install-PoshPseudoServiceManagement -Verbose -Confirm -WhatIf
